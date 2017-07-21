ab2b522d109de7a1b3e3993ac410e99637b931af
Heather Sosa <heatherlsosa@gmail.com>
2017-07-21 10:09:01 -0600

    read-file working

--- liri.js
+++ liri.js
@@ -1,5 +1,3 @@
-//LOOK INTO SWITCH() instead of all these if statements.... 07/18/17
-
 var sourceFile = require('./keys.js'); //links or 'requires' this JS file
 //REQUIRED NODE MODULES
 var Twitter = require('twitter');
@@ -22,52 +20,50 @@ var spotify = new Spotify({
 var nodeArg = process.argv[2];
 var nodeQuery = process.argv.slice(3).join(' ');
 
+function runThisSwitch() {
+	switch (nodeArg) {
+	  case "spotify-this-song":
+	    spotifySearch();
+	    break;
+
+	  case "my-tweets":
+	  	myTwitter();
+	  	break;
+	  
+	    case "movie-this":
+	  	movieSearch();
+	  	break;
+	  
+	  case "do-what-it-says":
+	  	doThis();
+	  	break;
+	  }
+};
+
+runThisSwitch();
+
 //TWITTER SEARCH
-if (nodeArg === 'my-tweets') {
+function myTwitter() {
 	var params = {screen_name: 'MediaGeek303', count: '20'};
 
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
    // console.log(tweets);
 	   for (var i = 0; i < tweets.length; i++) {
-		   console.log("Tweet " + (i+1) + ": " + tweets[i].text)
-		   console.log("====================")
-   		}
+		   console.log("Tweet " + (i+1) + ": " + tweets[i].text + "\r\n====================");
+		}
 	});
 };
-
 //SPOTIFY SEARCH
-if (nodeArg === 'spotify-this-song' && nodeQuery === '') {
-	
-		spotify.search({ type: 'track', query: 'I saw the sign', limit: '1'}, function(err, data) {
-		if (err) {
-	    	return console.log('Error occurred: ' + err);
-		}
-		// console.log(JSON.stringify(data, null, 2));
-			var singleSong = "You didn't search anything, so here's this one... \r\nArtists: " + JSON.stringify(data.tracks.items[i].artists[0].name) + 
-				"\r\nSong name: " + JSON.stringify(data.tracks.items[i].name) + "\r\nPreview Link: " + 
-				JSON.stringify(data.tracks.items[i].external_urls.spotify) + "\r\nAlbum: " + 
-				JSON.stringify(data.tracks.items[i].album.name) + 
-				'\r\n===============================================================\r\n';
-
-			console.log(singleSong);
-			// write data to file
-			fs.appendFile("log.txt", singleSong, function(error, data) {
-			  if (error) {
-			    return console.log(error);
-			  	} // console.log("file updated!");
-			});
-		});
-	};
-
-if (nodeArg === 'spotify-this-song' && nodeQuery != '') {
-
+function spotifySearch() {
+	if (nodeQuery === '') {
+		nodeQuery = 'I-saw-the-sign'
+	}
 	spotify.search({ type: 'track', query: nodeQuery, limit: '5'}, function(err, data) {
 		if (err) {
 	    	return console.log('Error occurred: ' + err);
 		}
-		// console.log(JSON.stringify(data, null, 2));
+
 		for (var i = 0; i < data.tracks.items.length; i++) {
-			// console.log(JSON.stringify(data, null, 2 ));
 			var songInfo = "Artists: " + JSON.stringify(data.tracks.items[i].artists[0].name) + 
 				"\r\nSong name: " + JSON.stringify(data.tracks.items[i].name) + "\r\nPreview Link: " + 
 				JSON.stringify(data.tracks.items[i].external_urls.spotify) + "\r\nAlbum: " + 
@@ -82,17 +78,22 @@ if (nodeArg === 'spotify-this-song' && nodeQuery != '') {
 			  	} // console.log("file updated!");
 			});
 		};
-	})
+	});
 };
 
+function doThis() {
   fs.readFile("random.txt", "utf8", function(err, data) {
     if (err) {
       return console.log(err);
     }
-    data = data.split(",");
-    process.argv[2] = data[0];
-    process.argv[3] = data[1];
+    var splitData = data.split("\r\n");
 
-    console.log (process.argv[2] + " - " + process.argv[3]);
+      for (var i = 0; i < splitData.length; i++) {
+	    parseData = splitData[i].split(",");
+	    nodeArg= parseData[0];
+	    nodeQuery = parseData[1];
 
+		runThisSwitch(nodeArg);
+		}
   });
+};
